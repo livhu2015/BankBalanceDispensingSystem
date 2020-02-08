2ccfc33f045eb138a1360e18dfca1d361a21c52e
Livhuwani Matsigila <livhuwanimatsigila@gmail.com>
2020-02-08 17:18:49 +0200

    [Livhu] -  adding unit test

---
+++ src/test/java/com/discoveybank/balancedispensing/TransactionalServiceImpTest.java
@@ -0,0 +1,13 @@
+package com.discoveybank.balancedispensing;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TransactionalServiceImpTest {
+
+    @Test
+    void displayBalance() {
+    }
+}
